0a0637b6d90378bdd4c5f367244811b37640f688
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-05 15:52:02 -0500

    squares rotate around center instead of themselves

--- sketch.js
+++ sketch.js
@@ -3,17 +3,24 @@ function setup() {
     frameRate(60);
   }
    
+let squareSpacing = 100
+let squareWidth = 25
 function draw() {
   background(20, 20, 20, 40);
   translate(width/2, height/2);
   rotate(radians(frameCount));
+
   push();
   colorMode(HSB);
   rectMode(CENTER);
   stroke(frameCount/2, 100, 100);
   strokeWeight(2);
   fill(255);
-  rect(0, 0, 100, 100);
+  for (let posX = 3 * -width/2; posX <= 3 * width/2; posX += squareSpacing) {
+    for (let posY = 3 * -height/2; posY <= 3 * height/2; posY += squareSpacing) {
+      square(posX, posY, squareWidth);
+    }
+  }
   pop();
 
 }
