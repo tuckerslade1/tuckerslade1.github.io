0a0773177f30d0e735c02568852a0425ee7ba016
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-05 16:13:31 -0500

    fixed coloring of squares

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
     createCanvas(windowWidth-20, windowHeight-126);
     frameRate(60);
-}
+  }
    
 let squareSpacing = 100
-let squareWidth = 25
+let squareWidth = 35
 function draw() {
   background(20, 20, 20, 40);
   translate(width/2, height/2);
@@ -13,12 +13,16 @@ function draw() {
   push();
   colorMode(HSB);
   rectMode(CENTER);
-  stroke((frameCount/2) % 360, 100, 100);
   strokeWeight(2);
   fill(255);
+  let xCount = 0;
   for (let posX = 3 * -width/2; posX <= 3 * width/2; posX += squareSpacing) {
+    let yCount = 0;
+    xCount++;
     for (let posY = 3 * -height/2; posY <= 3 * height/2; posY += squareSpacing) {
+      stroke((xCount * yCount) % 360, 100, 100);
       square(posX, posY, squareWidth);
+      yCount++;
     }
   }
   pop();
