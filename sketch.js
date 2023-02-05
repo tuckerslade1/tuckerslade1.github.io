0e7ff30c8f1849f7f65f53606c95bac3fe59064f
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-05 15:46:56 -0500

    fixed indentation and windowHeight

--- sketch.js
+++ sketch.js
@@ -1,21 +1,19 @@
 function setup() {
-    createCanvas(windowWidth-20, windowHeight-122);
+    createCanvas(windowWidth-20, windowHeight-126);
     frameRate(60);
   }
-  
-    // TODO: Draw horizontal and vertical guidelines
-  
-  function draw() {
-    background(20, 20, 20, 40);
-    translate(width/2, height/2);
-    rotate(radians(frameCount));
-    push();
-    colorMode(HSB);
-    rectMode(CENTER);
-    stroke(frameCount/2, 100, 100);
-    strokeWeight(2);
-    fill(255);
-    rect(0, 0, 100, 100);
-    pop();
-  
-  }
+   
+function draw() {
+  background(20, 20, 20, 40);
+  translate(width/2, height/2);
+  rotate(radians(frameCount));
+  push();
+  colorMode(HSB);
+  rectMode(CENTER);
+  stroke(frameCount/2, 100, 100);
+  strokeWeight(2);
+  fill(255);
+  rect(0, 0, 100, 100);
+  pop();
+
+}
