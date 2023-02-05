f1b6df2ca5d9adc9dbbc3287accce5f7d4490d6d
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-05 15:40:56 -0500

    added square with trail

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth-20, windowHeight-122);
+    frameRate(60);
   }
   
     // TODO: Draw horizontal and vertical guidelines
   
   function draw() {
+    background(20, 20, 20, 40);
     translate(width/2, height/2);
+    rotate(radians(frameCount));
     push();
+    colorMode(HSB);
     rectMode(CENTER);
-    stroke(0)
-    strokeWeight(5)
-    fill(200)
+    stroke(frameCount/2, 100, 100);
+    strokeWeight(2);
+    fill(255);
     rect(0, 0, 100, 100);
     pop();
+  
   }
