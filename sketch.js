c5626ec185dbdd0140781f838249f249d338e06b
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-05 20:24:35 -0500

    trying to make cubes rotate in place

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 function setup() {
     createCanvas(windowWidth-4, windowHeight-126, WEBGL); // makes the canvas (between header and footer)
     frameRate(60);
+    colorMode(HSB);
+    strokeWeight(2); // thickness of outline
   }
    
 let squareSpacing = 100
@@ -8,25 +10,28 @@ let squareWidth = 35
 function draw() {
   background(20, 20, 20, 40);
 
-  //rotateX(radians(frameCount*1.2)); // rotating the grid of the canvas to make it look like squares are spinning
-  //rotateY(radians(frameCount));
-
   push();
-  translate(-width/2, -height/2) // start at top left corner
-  colorMode(HSB);
-  strokeWeight(2); // thickness of outline
   let row = 0;
-  for (let posX = 3 * -width/2; posX <= 3 * width/2; posX += squareSpacing) {
+  for (let posX = 0; posX <= width; posX += squareSpacing) {
     let column = 0;
     push(); // used to reset the row position after generating each row
-    for (let posY = 3 * -height/2; posY <= 3 * height/2; posY += squareSpacing) {
-      //let hue = ((row + column) * 40) % 360 // color of square determined by its row and column
-      let hue = map(row, 0, windowWidth/squareSpacing + 1, 0, 360)
+    for (let posY = 0; posY <= width; posY += squareSpacing) {
+      // rotating cubes based on mouse position
+      let x = posX - width/2
+      let y = posY - height/2
+      let angle = atan2((mouseY - height/2) - y, (mouseX - width/2) - x)
+      rotateX(angle)
+      rotateY(angle)
+      rotateZ(angle)
+
+      let hue = map(row, 0, windowWidth/squareSpacing + 1, 0, 360) // color of cube determined by its column
       stroke(0); // changes color of square outline
       fill(hue, 100, 100) // changes fill color of squares
+
       box(squareWidth); // makes a square
       translate(0, squareSpacing) // move across rows
       column++;
+      
     }
     pop(); // used to reset the row position after generating each row
     row++;
@@ -34,4 +39,8 @@ function draw() {
   }
   pop();
 
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth-4, windowHeight-126);
 }
