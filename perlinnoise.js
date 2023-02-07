962f023697eeecb5be3960a40f827b0ed0101eb2
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-07 13:26:16 -0500

    initial commit

--- perlinnoise.js
+++ perlinnoise.js
@@ -1,19 +1,15 @@
 function setup() {
-    createCanvas(windowWidth-4, windowHeight-126); // makes the canvas (between header and footer)
+    createCanvas(windowWidth-4, windowHeight-4); // makes the canvas (between header and footer)
     frameRate(60);
     strokeWeight(1); // thickness of outline
 }
 
-let spacing = 30;
-
 function draw() {
-    background(20, 20, 20);
-    for (x = width/2; x >=width/4; x--) {
+    colorMode(HSB);
+    background(300,6,96,10);
+    for (x=100; x<=width-100; x++) {
         push();
-        colorMode(HSB);
-        stroke(map(noise(x),0,1,185,215), map(noise(x),0,1,80,100), 100);
-        noFill();
-        circle(width/2,height/2,(width/2)-x);
+
         pop();
     }
 }
