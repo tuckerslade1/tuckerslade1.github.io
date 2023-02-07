4bbd5d31c7c41933ad5c835ac6457f23ec70090e
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-06 20:42:37 -0500

    smaller circle

--- perlinnoise.js
+++ perlinnoise.js
@@ -8,10 +8,10 @@ let spacing = 30;
 
 function draw() {
     background(20, 20, 20);
-    for (x = width/2; x >=0; x-=10) {
+    for (x = width/2; x >=width/4; x--) {
         push();
         colorMode(HSB);
-        stroke(map(noise(x),0,1,185,215), noise(x)*100, 100);
+        stroke(map(noise(x),0,1,185,215), map(noise(x),0,1,80,100), 100);
         noFill();
         circle(width/2,height/2,(width/2)-x);
         pop();
