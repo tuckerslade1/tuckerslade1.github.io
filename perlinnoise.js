bc4a1dacb51f48f6d4bfd0a7d55c1d5841bdfcd0
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-07 18:18:05 -0500

    renamed variables

--- perlinnoise.js
+++ perlinnoise.js
@@ -1,21 +1,22 @@
 const xMargin = 0; // margin from edge of screen
 const yMargin = 0;
-const xSpacing = 12; // space between each point
-const ySpacing = 12;
+const squareSize = 10; // size of each square
 const xWidth = 1920; // size of design
 const yWidth = 1080;
 const variability = 0.1 // range [0,1], lower value = less variable
-const pointSize = 1 // size of points
 let dotArray = [];
 let pointCoords;
 let xPos;
 let yPos;
 let transparency;
-const minTransparency = -0.3;
-const maxTransparency = 1.2;
+const minTransparency = -0.05;
+const maxTransparency = 1.0;
 let saturation;
 const minSaturation = 0;
 const maxSaturation = 110;
+let brightness;
+const minBrightness = 0;
+const maxBrightness = 50;
 let t = 0;
 
 function setup() {
@@ -24,9 +25,9 @@ function setup() {
     rectMode(CENTER);
 
     // making array 1.2x larger than screen so screen will always be full
-    for (let x=-xWidth/2; x<=xWidth/2; x+=xSpacing) { // creating an array with coordinates of every point in dot field
+    for (let x=-xWidth/2; x<=xWidth/2; x+=squareSize) { // creating an array with coordinates of every point in dot field
         dotArray.push([]); // add empty array for each column of dots
-        for (let y=-yWidth/2; y<=yWidth/2; y+=ySpacing) { // making array 1.2x larger than screen so screen will always be full
+        for (let y=-yWidth/2; y<=yWidth/2; y+=squareSize) { // making array 1.2x larger than screen so screen will always be full
             dotArray[dotArray.length-1].push([x,y]); // add coordinates of each point to the last item in dotArray
         }
     }
@@ -39,18 +40,19 @@ function draw() {
         xPos = dotArray[x][0][0];
         for (let y=0; y<dotArray[0].length; y++) { // iterating over y-values of points in dotArray
             yPos = dotArray[x][y][1];
-            transparency = map(noise((xPos+xWidth/2)/300,(yPos+yWidth/2)/300,t+15),0,1,minTransparency,maxTransparency) // can map to brightness levels < 0 and > 100 to increase numbers of very dark and very bright points
-            saturation = map(transparency,minTransparency,maxTransparency,minSaturation,maxSaturation);
+            //transparency = map(noise((xPos+xWidth/2)/300,(yPos+yWidth/2)/300,t+15),0,1,minTransparency,maxTransparency) // can map to brightness levels < 0 and > 100 to increase numbers of very dark and very bright points
+            saturation = map(noise((xPos+xWidth/2)/300,(yPos+yWidth/2)/300,t+15),0,1,minSaturation,maxSaturation);
+            brightness = map(saturation,minSaturation,maxSaturation,minBrightness,maxBrightness);
             push();
-            stroke(0,saturation,255,transparency);
-            strokeWeight(pointSize);
-            point(xPos+xMargin,yPos+yMargin);
+            stroke(0,saturation,brightness,1);
+            strokeWeight(squareSize);
+            square(xPos+xMargin,yPos+yMargin,squareSize);
             pop();
         }
     }
-    t+=0.03
+    t+=0.01
 }
 
 function windowResized(){
-    resizeCanvas(windowWidth-12, windowHeight-12);
+    resizeCanvas(windowWidth, windowHeight);
 }
