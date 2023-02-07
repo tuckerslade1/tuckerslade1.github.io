7fb86d43ae748803251958a893022de0dbe28ed1
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-07 15:29:58 -0500

    changed to circles

--- perlinnoise.js
+++ perlinnoise.js
@@ -1,25 +1,26 @@
-const xMargin = 100;
-const yMargin = 50;
-const xSpacing = 10;
-const ySpacing = 10;
-const variability = 10 // how many pixels away a point can be from its original position before noise
+const xMargin = 0;
+const yMargin = 0;
+const xSpacing = 100;
+const ySpacing = 100;
+const variability = 0.1 // range [0,1], lower value = less variable
 let dotArray = [];
 let pointCoords;
 let xPos;
 let yPos;
-console.log(dotArray);
+let t = 0;
 
 function setup() {
     createCanvas(windowWidth-4, windowHeight-4); // makes the canvas (between header and footer)
-    frameRate(60);
-    for (let x=0; x<=width-(2*xMargin); x+=xSpacing) { // creating an array with coordinates of every point in dot field
+    // making array 4x larger than screen so screen will always be full
+    for (let x=0; x<=4*(width-(2*xMargin)); x+=xSpacing) { // creating an array with coordinates of every point in dot field
         dotArray.push([]); // add empty array for each column of dots
-        for (let y=0; y<=height-(2*yMargin); y+=ySpacing) {
+        for (let y=0; y<=4*(height-(2*yMargin)); y+=ySpacing) { // making array 4x larger than screen so screen will always be full
             dotArray[dotArray.length-1].push([x,y]); // add coordinates of each point to the last item in dotArray
         }
     }
 }
 
+/*
 // debugging dotArray
 for (i=0;i<dotArray.length;i++) {
     for (j=0; j<dotArray[i].length;j++) {
@@ -28,22 +29,25 @@ for (i=0;i<dotArray.length;i++) {
         }
     }
 }
+*/
 
 function draw() {
     colorMode(HSB);
-    background(60,6,96);
+    background(60,3,90, 0.3);
     for (let x=0; x<dotArray.length; x++) {
+        //xPos = x*xSpacing;
+        //xPos = map(noise(xPos,frameCount),0,1,xPos-variability, xPos+variability);
+        xPos = dotArray[x][0][0] * map(noise(t+15),0,1,1-variability,1+variability);
         for (let y=0; y<dotArray[0].length; y++) {
-            xPos = dotArray[x][y][0];
-            xPos = map(noise(xPos),0,1,xPos-variability, xPos+variability);
-            yPos = dotArray[x][y][1];
-            yPos = map(noise(yPos),0,1,yPos-variability,yPos+variability);
+            //yPos = dotArray[x][y][1];
+            //yPos = map(noise(yPos,frameCount),0,1,yPos-variability,yPos+variability);
+            yPos = dotArray[x][y][1] * map(noise(t+5),0,1,1-variability,1+variability);
             push();
-            stroke(217,2,7);
-            strokeWeight(1);
-            point(xPos +xMargin,yPos+yMargin);
+            stroke(0,2,6);
+            strokeWeight(20);
+            point(xPos+xMargin,yPos+yMargin);
             pop();
         }
     }
-    console.log(xPos.toString());
+    t+=0.005
 }
