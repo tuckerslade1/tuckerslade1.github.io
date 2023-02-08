7bd1fc20fe5efbee9801bb9ee30a1318f739a45b
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-07 23:05:39 -0500

    point in 3d space

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight); // makes the canvas (between header and footer)
+    createCanvas(windowWidth, windowHeight, WEBGL); // makes the canvas (between header and footer)
     frameRate(60);
 }
 
-let x=0;
+let x=-100;
 let y=0;
 let z=0;
 let t=0;
 
 function draw() {
-  background(20, 20, 20, 40);
-  translate(width/2, height/2);
+  background(20, 20, 20, 255);
   stroke(255,255,255);
-  strokeWeight(40);
-  point(x,y);
-  t+=0.005
+  strokeWeight(10);
+  point(x,map(noise(x,y),0,1,-100,100), 100);
+  orbitControl();
+  x+=1
 }
