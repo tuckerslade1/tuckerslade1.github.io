7d9c632a2bdaded4c57bc2bc9c467552791ae2fd
Tucker <117668459+tuckerslade1@users.noreply.github.com>
2023-02-07 23:17:13 -0500

    colored points based on z value

--- sketch.js
+++ sketch.js
@@ -9,10 +9,12 @@ let z=0;
 let t=0;
 
 function draw() {
-  background(20, 20, 20, 255);
-  stroke(255,255,255);
-  strokeWeight(10);
-  point(x,map(noise(x,y),0,1,-100,100), 100);
+  x=map(noise(x,t),0,1,-100,100);
+  y=map(noise(y,t),0,1,-100,100);
+  z=map(noise(z,t),0,1,-100,100);
+  stroke(255,255,z+155);
+  strokeWeight(2);
+  point(x,y,z);
   orbitControl();
-  x+=1
+  t+=0.0001
 }
